b31f18be33e66022119ef4b8c696007e0a38f13e
Bernardo Gabriel Mejía Pérez <bernardo.mejiaperez703@gmail.com>
2024-04-04 22:48:18 -0600

    Article index

---
+++ AppBlogCore/Areas/Admin/Controllers/ArticlesController.cs
@@ -0,0 +1,34 @@
+﻿using AppBlogCore.Data;
+using AppBlogCore.DataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppBlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArticlesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ArticlesController(ApplicationDbContext context, IUnitOfWork unitOfWork)
+        {
+            _context = context;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitOfWork.Article.GetAll() });
+        }
+
+        #endregion
+    }
+}
