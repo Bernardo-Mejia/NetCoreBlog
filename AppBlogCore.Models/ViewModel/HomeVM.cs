6839f216dfa9cd0050de247991a328ae6a96b937
Bernardo Gabriel Mejía Pérez <bernardo.mejiaperez703@gmail.com>
2024-08-18 13:58:46 -0600

    List Products Home

---
+++ AppBlogCore.Models/ViewModel/HomeVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBlogCore.Models.ViewModel
+{
+    public class HomeVM
+    {
+        public IEnumerable<Slider> SlidersList { get; set; }
+        public IEnumerable<Article> ArticlesList { get; set; }
+    }
+}
